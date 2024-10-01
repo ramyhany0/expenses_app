80d81a6e7df72a35e2800ade44fe5e162fc39849
ramyhany0 <ramyhany161@gmail.com>
2024-10-01 13:09:46 +0300

    "Added uuid import and initialized id with uuid.v4() in Expense constructor"

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,5 +1,7 @@
 import 'package:uuid/uuid.dart';
 
+const uuid = Uuid();
+
 class Expense {
   final String id;
   final String title;
@@ -7,9 +9,8 @@ class Expense {
   final DateTime date;
 
   Expense({
-    required this.id,
     required this.title,
     required this.amount,
     required this.date,
-  });
+  }) : id = uuid.v4();
 }
