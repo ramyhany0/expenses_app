22d682eb880b238479980942974c3bf550cd6fda
ramyhany0 <ramyhany161@gmail.com>
2024-10-01 13:16:46 +0300

    "Added Category enum and property to Expense class, and created an instance of Expense (V1)"

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,26 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, work }
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
   Expense({
+    required this.category,
     required this.title,
     required this.amount,
     required this.date,
   }) : id = uuid.v4();
 }
+
+var V1 = Expense(
+  category: Category.work,
+  title: "Flutter Course",
+  amount: 29.9,
+  date: DateTime.now(),
+);
